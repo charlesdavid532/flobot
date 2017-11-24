4fbaeeb539723f467d012aaa312e605b15cf66c7
David <cdavid@deloitte.com>
2017-11-24 20:44:45 +0530

    Fix indentation issue in date_utils

--- date_utils.py
+++ date_utils.py
@@ -23,23 +23,23 @@ class DateUtils(object):
 	    return DateUtils.getFutureDateAndTimeMins(mins).strftime("%Y-%m-%d %H:%M:%S")
 
 	@staticmethod
-    def getFutureDateAndTimeSecs(secs):
-        return DateUtils.getCurrentDateAndTime() + timedelta(seconds=secs)
+	def getFutureDateAndTimeSecs(secs):
+		return DateUtils.getCurrentDateAndTime() + timedelta(seconds=secs)
 
-    @staticmethod
-    def getStrFutureDateAndTimeSecs(secs):
-        return DateUtils.getFutureDateAndTimeSecs(secs).strftime("%Y-%m-%d %H:%M:%S")
+	@staticmethod
+	def getStrFutureDateAndTimeSecs(secs):
+		return DateUtils.getFutureDateAndTimeSecs(secs).strftime("%Y-%m-%d %H:%M:%S")
 
 
-    '''
-    Compares date time 1 with date time 2
-    Returns True if 1 < 2
-    Else False
-    '''
-    @staticmethod
-    def compareDateAndTime(dateTime1, dateTime2):
-        if dt.strptime(dateTime1, "%Y-%m-%d %H:%M:%S")  < dt.strptime(dateTime2, "%Y-%m-%d %H:%M:%S"):
-            return True
-        else:
-            return False
+	'''
+	Compares date time 1 with date time 2
+	Returns True if 1 < 2
+	Else False
+	'''
+	@staticmethod
+	def compareDateAndTime(dateTime1, dateTime2):
+		if dt.strptime(dateTime1, "%Y-%m-%d %H:%M:%S")  < dt.strptime(dateTime2, "%Y-%m-%d %H:%M:%S"):
+			return True
+		else:
+			return False
 		
