0b24c7db79b46f0cf6e04e316cfcc33da1f3ac0e
David <cdavid@deloitte.com>
2017-12-10 12:44:08 +0530

    Update welcome

--- welcome.py
+++ welcome.py
@@ -1,29 +1,39 @@
 from card import Card
 from constants import Constants
 from suggestion_chip import SuggestionChip
-from flask import session
 class WelcomeResponse(object):
 	"""Handles the welcome request"""
 	def __init__(self, requestData):
 		super(WelcomeResponse, self).__init__()
 		self.requestData = requestData
 		self.userDataObj = None
+		self.source = None
 
 	def setUserData(self, userDataObj):
 		self.userDataObj = userDataObj
+
+	def setSource(self, source):
+		self.source = source
 
 	def getWelcomeResponse(self):
 		print ("Inside show welcome intent")
 		
 
 		simpleResponse = []
-		simpleResponse.append("Hi " + self.userDataObj.getUsername() + "! I am Flobot")
+
+		gUsername = self.userDataObj.getUsername()
+
+		if gUsername == False:
+			gUsername = ""
+
+		simpleResponse.append("Hi " + gUsername + "! I am Flobot")
 		simpleResponse.append("What would you like to know?")
 
 		sugList = []
 		sugList.append("Offers")
 		sugList.append("Free delivery")
-		welcomeSuggestionChipObj = SuggestionChip(simpleResponse)
+		SuggestionChip.set_provider_none()
+		welcomeSuggestionChipObj = SuggestionChip.get_provider(self.source, simpleResponse)
 		welcomeSuggestionChipObj.addSugTitles(sugList)
 		return welcomeSuggestionChipObj.getSuggestionChipResponse()
 		'''
