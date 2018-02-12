25587801ccf124ce43d515bbcb58298ee682d373
David <cdavid@deloitte.com>
2018-02-12 16:46:07 +0530

    Add flask admin create form

--- offers/create_offer_form_view.py
+++ offers/create_offer_form_view.py
@@ -1,43 +1,25 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, ValidationError
-from wtforms.fields.html5 import TelField
-from wtforms.validators import DataRequired, NumberRange
-import re
-from utils.date_utils import DateUtils
+from flask_admin.contrib.pymongo import ModelView
+from offers.coupon_list_form import CouponListForm
 from utils.utils import Utils
-import phonenumbers
-from phonenumbers.phonenumberutil import NumberParseException
-from phonenumbers import NumberParseException
-from wtforms.fields.html5 import DateField
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from werkzeug.utils import secure_filename
 from utils.date_utils import DateUtils
 from constants import Constants
 from common.amazon_s3 import AmazonS3
 from bson.decimal128 import Decimal128
+from werkzeug.utils import secure_filename
+class CreateOfferFormView(ModelView):
+	column_list = ('percentOff', 'minBillAmount', 'startedAt', 'expiresAt', 'offerImage', 'offerCode', 'offerTitle', 'offerText')
+	form = CouponListForm
 
+	def on_model_change(self, form, model, is_created):
+		print("model is:::" + str(model))
+		self.validateAndCreateOffer(form, model)
 
-
-class CreateOfferForm(FlaskForm):
-	percentOff = DecimalField('Percentage Off', validators=[DataRequired("Percentage off is required"), 
-															NumberRange(min=0, max=100, message="Percentage should be between 0 and 100")])	
-	minBillAmount = DecimalField('Bill Amount')
-	startedAt = DateField('Start Date', format='%Y-%m-%d')
-	expiresAt = DateField('Expires At', format='%Y-%m-%d')
-	offerImage = FileField(validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
-	offerCode = StringField('Offer code', validators=[DataRequired("Offer code is required")])
-	offerTitle = StringField('Offer Title', validators=[DataRequired("Offer title is required")])
-	offerText = StringField('Offer Text', validators=[DataRequired("Offer text is required")])
-	"""docstring for NutritionDetailedController"""
-	def __init__(self, mongo):
-		super(CreateOfferForm, self).__init__()
-		self.mongo = mongo
 
 
 	'''
 	Validates whether all details entered are correct and creates the offer in the database
 	'''
-	def validateAndCreateOffer(self, createOfferForm):
+	def validateAndCreateOffer(self, createOfferForm, model):
 		offerStartDate = createOfferForm.startedAt.data
 		offerStartDateStr = DateUtils.convertDateStrToDateTimeStr(str(offerStartDate))
 		offerExpiresAtDate = createOfferForm.expiresAt.data
@@ -47,15 +29,16 @@ class CreateOfferForm(FlaskForm):
 		print("offerExpiresAtDateStr::" + str(offerExpiresAtDateStr))
 
 		if DateUtils.compareDateAndTime(offerExpiresAtDateStr, offerStartDateStr) == True:
-			return 'Start date cannot be greater than end date'
+			#return 'Start date cannot be greater than end date'
+			raise validators.ValidationError("Start date cannot be greater than end date")
 
-		self.createOfferAndStoreInDB(createOfferForm, offerStartDateStr, offerExpiresAtDateStr)
+		self.createOfferAndStoreInDB(createOfferForm, offerStartDateStr, offerExpiresAtDateStr, model)
 
 		print("Offer is created")
-		return "Offer Created"
+		#return "Offer Created"
 
-	def createOfferAndStoreInDB(self, createOfferForm, offerStartDateStr, offerExpiresAtDateStr):
-		listCouponData = self.mongo.db.couponList
+	def createOfferAndStoreInDB(self, createOfferForm, offerStartDateStr, offerExpiresAtDateStr, model):
+		#listCouponData = self.mongo.db.couponList
 
 		fileData = createOfferForm.offerImage.data
 		filename = secure_filename(fileData.filename)
@@ -75,22 +58,28 @@ class CreateOfferForm(FlaskForm):
 
 		print(strofferMinBillAmount, type(strofferMinBillAmount))
 		print(strofferPercentOff, type(strofferPercentOff))
-		
+		'''
 		# Inserting the offers data into the db
 		listCouponData.insert({
 			'offerCode' : createOfferForm.offerCode.data,
 			'offerTitle' : createOfferForm.offerTitle.data,
 			'offerText' : createOfferForm.offerText.data,
-			'minBillAmount' : str(createOfferForm.minBillAmount.data),
+			'minBillAmount' : str(createOfferForm.minbillAmount.data),
 			'startedAt': offerStartDateStr,
 			'expiresAt' : offerExpiresAtDateStr,
 			'offerImage' : filename,
 			'percentOff' : str(createOfferForm.percentOff.data)
 			})
+		'''
+
+		model['minBillAmount'] = str(createOfferForm.minBillAmount.data)
+		model['startedAt'] = offerStartDateStr
+		model['expiresAt'] = offerExpiresAtDateStr
+		model['offerImage'] = filename
+		model['percentOff'] = str(createOfferForm.percentOff.data)
 
 
 	def saveImageToAws(self, fileData, filename):       
 		myAmazonS3 = AmazonS3(Constants.getAWSBucketName())
 		myAmazonS3.saveResourceToAWS(fileData, Constants.getAWSCouponImagesBucketName() + '/' + filename, 
 			Utils.getImageContentType(filename), Constants.getAWSBucketName())
-		
