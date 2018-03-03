e02bb248718352dbd25694c253a25e1511de68c0
David <cdavid@deloitte.com>
2018-03-03 20:12:19 +0530

    Add broadcasting feature basics

--- broadcastMsg/non_prom_broadcast_form.py
+++ broadcastMsg/non_prom_broadcast_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, DateField, RadioField, ValidationError
+from wtforms import StringField, HiddenField, DecimalField, DateField, RadioField, ValidationError
 #from wtforms.fields.html5 import DateTimeField
 from wtforms.fields.html5 import TelField
 from wtforms.validators import DataRequired
@@ -9,9 +9,12 @@ import phonenumbers
 from phonenumbers.phonenumberutil import NumberParseException
 from phonenumbers import NumberParseException
 import decimal
+import requests
+import json
 
 class NonPromBroadCastForm(FlaskForm):
-	data = StringField('Message Content', render_kw={'disabled':'disabled'})	
+	#data = StringField('Message Content', render_kw={'disabled':'disabled'})	
+	data = HiddenField('Message Content')	
 	messageUsers = StringField('Select the users to broadcast', validators=[DataRequired("Users are required")])
 	messageTiming = RadioField('Message Timing', choices=[('0','Send Now'),('1','Send Later')])
 	messageTimingDateTimeWidget = StringField('Select the date and time to broadcast')
@@ -30,6 +33,7 @@ class NonPromBroadCastForm(FlaskForm):
 	2. Checks whether the broadcast time is in the future
 	'''
 	def validateBroadcast(self, broadcastForm):
+		bMessageContent = broadcastForm.data.data
 		bMessageUsers = broadcastForm.messageUsers.data
 		bMessageUsers = bMessageUsers.replace(" ", "")
 		user_list = bMessageUsers.split(",")
@@ -61,9 +65,56 @@ class NonPromBroadCastForm(FlaskForm):
 			bDateTime = DateUtils.convertMDYtoYMD(bDateTime)
 			print("The date time string after converting to YMD:" + bDateTime)
 			if DateUtils.compareDateAndTime(bDateTime, DateUtils.getStrCurrentDateAndTime()) == True:
-					return 'Broadcast time is in the past'
+				return 'Broadcast time is in the past'
+
+
+		print("The message content is::" +bMessageContent)
+		self.createBroadcast(bMessageContent)
 
 		return 'Valid Broadcast'
+
+
+	def createBroadcast(self, bMessageContent):
+		
+
+		headers = {
+		    'Content-Type': 'application/json',
+		}
+
+		params = (
+		    ('access_token', 'EAACtGhC8ZAjsBALnvZAR60H8hZCJcAh5LF5MBZCCFKKFZBxHOW0ERQDo0dGAZAqvEzWEi9iuYlaKy7rZBNDWin92yKfcSdceeEdUfRvIniHednSIYlVJgMLk9p0XZBsWLQ4P7bmZBrG20Tg2aFoMAto0uUZAafKg9vNBDM8wKWtmq4mgZDZD'),
+		)
+
+		#data = '{    \n  "messages": [\n    {\n      "attachment":{\n        "type":"template",\n        "payload":{\n          "template_type":"generic",\n          "elements":[\n             {\n              "title":"Welcome to Our Marketplace!!",\n              "image_url":"https://www.facebook.com/jaspers.png",\n              "subtitle":"Fresh fruits and vegetables. Yum.",\n              "buttons":[\n                {\n                  "type":"web_url",\n                  "url":"https://www.jaspersmarket.com",\n                  "title":"View Website"\n                }              \n              ]      \n            }\n          ]\n        }       \n      }\n    }\n  ]\n}'
+		data = bMessageContent
+
+		response = requests.post('https://graph.facebook.com/v2.11/me/message_creatives', headers=headers, params=params, data=data)
+
+		print(str(response))
+		print(response.content)
+		jsonResponse = json.loads(response.content.decode('utf-8'))
+		print(str(jsonResponse))
+
+		self.sendBroadcast(jsonResponse["message_creative_id"]);
+
+
+	def sendBroadcast(self, msgCreativeId):
+
+		headers = {
+		    'Content-Type': 'application/json',
+		}
+
+		params = (
+		    ('access_token', 'EAACtGhC8ZAjsBALnvZAR60H8hZCJcAh5LF5MBZCCFKKFZBxHOW0ERQDo0dGAZAqvEzWEi9iuYlaKy7rZBNDWin92yKfcSdceeEdUfRvIniHednSIYlVJgMLk9p0XZBsWLQ4P7bmZBrG20Tg2aFoMAto0uUZAafKg9vNBDM8wKWtmq4mgZDZD'),
+		    
+		)
+
+		data = '{    \n  "message_creative_id":' + msgCreativeId + ',\n  "notification_type": "REGULAR",\n}'
+
+		response = requests.post('https://graph.facebook.com/v2.11/me/broadcast_messages', headers=headers, params=params, data=data)
+
+		print(str(response))
+		print(response.content)
 
 	
 				
--- static/js/non-prom-broadcast.js
+++ static/js/non-prom-broadcast.js
@@ -15,12 +15,16 @@ var NonPromBroadcastView = Backbone.View.extend({
 
     initialize: function initialize() {
         this.quickReplyViewList = [];
+        this.btnViewList = [];
+        this.mainContentViewList = [];
+        this.mainContentElement = null;
         this.render();
     },
 
     events: {
         'change input[type=radio]': 'onMessageTimingBtnClicked',
-        'click #non-prom-msg': 'onNonPromMsgClicked'
+        'click #non-prom-msg': 'onNonPromMsgClicked',
+        'click #offer-submit': 'onBroadcastBtnClicked'
 
     },
 
@@ -56,25 +60,170 @@ var NonPromBroadcastView = Backbone.View.extend({
         $.get('/admin/non-prom-broadcast/');
     },
 
+    /**
+
+
+    Text Section
+    
+
+    */
+
     onTextClicked: function (ev) {
         console.log('Text button event detected in main view');
-        
+        this.mainContentElement = 'text';
+        this.mainContentViewList = [];
+        this.generateNewText();
     },
+
+    generateNewText: function generateNewText() {
+        var textModel = new TextData();
+        textModelId = this.generateRandomNum(1000,10000);
+        textModel.setNumId(textModelId);
+
+        this.injectTextHtml(textModelId);
+
+        var textView = new TextView({ model: textModel, el: '#' + textModelId.toString() });
+
+        this.listenTo(textView, 'TEXT_CLOSE_BTN_CLICKED', this.onTextCloseBtnClicked);
+        this.mainContentViewList.push(textView);
+
+    },
+
+
+    injectTextHtml: function injectTextHtml(textId) {
+        this.$el.find('.broadcast-builder-content-holder').html(this.createTextHtml(textId));
+        var self = this,
+            textContainer = this.$el.find('#' + textId);
+
+        textContainer.find('.text-close-button').on("click","", textId, function(ev) {
+            self.onTextCloseBtnClicked(ev.data);
+        });
+    },
+
+    createTextHtml: function createTextHtml(textId) {
+        return "<div id='" + textId + "' class='text-elem'><div class='text-text' contentEditable='true'></div><div class='text-close-button'>x</div></div>"
+    },
+
+
+    onTextCloseBtnClicked: function onTextCloseBtnClicked(data) {
+        console.log("Inside onTextCloseBtnClicked of main view");
+        console.log("Data is::" + data);
+        // Destroying the appropriate view
+        for (var i = 0; i < this.mainContentViewList.length; i++) {
+            var curView = this.mainContentViewList[i];
+            if (data == curView.model.getNumId()) {
+                curView.remove();
+                this.mainContentViewList.splice(i, 1);
+                break;
+            }
+        }
+    },
+
+
+    /**
+
+
+    Media Section
+    
+
+    */
 
     onMediaClicked: function (ev) {
         console.log('Media button event detected in main view');
-        
+        this.mainContentElement = 'media';  
+        this.mainContentViewList = [];
     },
+
+
+    /**
+
+
+    Card Section
+    
+
+    */
 
     onCardClicked: function (ev) {
         console.log('Card button event detected in main view');
-        
+        this.mainContentElement = 'card';
+        this.mainContentViewList = [];
     },
+
+
+
+
+
+    /**
+
+
+    Button Section
+    
+
+    */
+
 
     onButtonClicked: function (ev) {
         console.log('Button event detected in main view');
+        if (this.btnViewList.length < 3) {
+            this.generateNewButton();
+        }
         
     },
+
+    generateNewButton: function generateNewButton() {
+        var btnModel = new ButtonData();
+        btnModelId = this.generateRandomNum(1000,10000);
+        btnModel.setNumId(btnModelId);
+
+        this.injectBtnHtml(btnModelId);
+
+        var btnView = new ButtonView({ model: btnModel, el: '#' + btnModelId.toString() });
+
+        this.listenTo(btnView, 'BUTTON_CLOSE_BTN_CLICKED', this.onBtnCloseBtnClicked);
+        this.btnViewList.push(btnView);
+
+    },
+
+    injectBtnHtml: function injectBtnHtml(btnId) {
+        this.$el.find('.broadcast-builder-buttons-holder').append(this.createBtnHtml(btnId));
+        var self = this,
+            btnContainer = this.$el.find('#' + btnId);
+
+        btnContainer.find('.btn-close-button').on("click","", btnId, function(ev) {
+            self.onBtnCloseBtnClicked(ev.data);
+        });
+    },
+
+    createBtnHtml: function createBtnHtml(btnId) {
+        return "<div id='" + btnId + "' class='btn-elem'><div class='btn-text' contentEditable='true'></div><div class='btn-close-button'>x</div></div>"
+    },
+
+    onBtnCloseBtnClicked: function onBtnCloseBtnClicked(data) {
+        console.log("Inside onBtnCloseBtnClicked of main view");
+        console.log("Data is::" + data);
+        // Destroying the appropriate view
+        for (var i = 0; i < this.btnViewList.length; i++) {
+            var curView = this.btnViewList[i];
+            if (data == curView.model.getNumId()) {
+                curView.remove();
+                this.btnViewList.splice(i, 1);
+                break;
+            }
+        }
+    },
+
+
+    /**
+
+
+    Quick Reply Section
+    
+
+    */
+
+
+
+
 
     onQuickReplyClicked: function onQuickReplyClicked(ev) {
         console.log('Quick Reply event detected in main view');
@@ -127,9 +276,125 @@ var NonPromBroadcastView = Backbone.View.extend({
                 break;
             }
         }
+    },
+
+
+
+    /**
+    
+    Broadcast section
+
+    */
+
+    onBroadcastBtnClicked: function onBroadcastBtnClicked(ev) {
+        //alert('Broadcast button clicked');
+        /*
+        ev.stopImmediatePropagation();
+        ev.stopPropagation();
+        ev.preventDefault();
+        */
+        //$('.message-content-hidden-container').val('abcdefgh');
+        //var qrJSONList = this.constructQRJSON();
+        //$('.message-content-hidden-container').val(JSON.stringify(qrJSONList));
+        var constructedJSON = this.constructJSON();
+        console.log("constructed json is::" + JSON.stringify(constructedJSON));
+        $('.message-content-hidden-container').val(JSON.stringify(constructedJSON));
+    },
+
+    /*
+    Checks if the main content is :
+    1. Text
+        a. If no button - Create sugList
+        b. Create button template
+    2. Media
+    3. Card
+    */
+    constructJSON: function constructJSON() {
+        if (this.mainContentElement == 'text') {
+            return this.constructTextJSON();
+        } else if(this.mainContentElement == 'media') {
+            return this.constructMediaJSON();
+        } else if(this.mainContentElement == 'card') {
+            return this.constructCardJSON();
+        }
+    },
+
+    constructTextJSON: function constructTextJSON() {
+        if (this.btnViewList.length > 0) {
+            return this.constructBtnTemplateJSON();
+        } else {
+            return this.constructSugListJSON();
+        }
+    },
+
+    constructSugListJSON: function constructSugListJSON() {
+        var sugListJSON = {};
+        //sugListJSON["source"] = "phillips-bot";
+        //sugListJSON["contextOut"] = [];
+        //sugListJSON["speech"] = this.$el.find('.text-text').html();
+        //sugListJSON["displayText"] = this.$el.find('.text-text').html();
+        //sugListJSON["data"] = {};
+
+        sugListJSON["messages"] = [];
+
+
+        //dataSugListJSON = sugListJSON["data"];
+        dataSugListJSON = sugListJSON["messages"];
+
+        messagesDict = {};
+        messagesDict["attachment"] = {};
+        attachmentDict = messagesDict["attachment"];
+
+        attachmentDict["type"] = "template";
+        attachmentDict["payload"] = {};
+        payloadDict = attachmentDict["payload"];
+        //facebookDict["facebook"] = {};
+        //dataSugListJSON["facebook"] = {};
+
+        //facebookSugListJSON = facebookDict["facebook"];
+        payloadDict["template_type"] = "generic";
+        payloadDict["elements"] = [];
+        elementsArr = payloadDict["elements"];
+
+        elementsDict = {};
+        elementsDict["title"] = this.$el.find('.text-text').html();
+        elementsDict["subtitle"] = "Dummy";
+        
+        elementsArr.push(elementsDict);
+
+        messagesDict["quick_replies"] = this.constructQRJSON();
+
+
+        dataSugListJSON.push(messagesDict);
+
+        return sugListJSON;
+    },
+
+    constructBtnTemplateJSON: function constructBtnTemplateJSON() {
+
+    },
+
+
+    constructMediaJSON: function constructMediaJSON() {
+
+    },
+
+    constructCardJSON: function constructCardJSON() {
+
+    },
+
+
+    constructQRJSON: function constructQRJSON() {
+        var qrJSONList = [];
+        for (var i = 0; i < this.quickReplyViewList.length; i++) {
+            var curView = this.quickReplyViewList[i];
+            qrJSONList.push(curView.getJSON());
+        }
+
+        console.log(JSON.stringify(qrJSONList));
+        return qrJSONList;
+
     }
-
-
 
     
 
@@ -235,9 +500,130 @@ var QuickReplyView = Backbone.View.extend({
     onCloseBtnClicked: function onCloseBtnClicked(ev) {
         console.log("Close button clicked");
         this.trigger('QUICK_REPLY_CLOSE_BTN_CLICKED', this.model.get('numId'));
+    },
+
+    getJSON: function getJSON() {
+        var qrJSON = {};
+        qrJSON["content_type"] = "text";
+        var title = this.$el.find('.qr-text').html();
+        console.log("The title is::" + title);
+        qrJSON["title"] = title;
+        qrJSON["payload"] = title;
+        return qrJSON;
     }
 
 });
+
+
+
+var ButtonData = Backbone.Model.extend({
+    defaults: function() {
+        return {
+            "numId": 888
+        }
+    },
+
+    getNumId: function getNumId() {
+        return this.get('numId');
+    },
+
+    setNumId: function setNumId(numId) {
+        this.set({'numId': numId});
+    },
+
+    initialize: function () {
+    }
+});
+
+
+var ButtonView = Backbone.View.extend({
+    model: ButtonData,
+    
+
+    initialize: function () {
+        this.render();
+    },
+
+    events: {        
+        'click .button-close-button': 'onCloseBtnClicked',
+        
+    },
+
+    render: function () {
+        console.log('Inside render of ButtonView');        
+    },
+
+    onCloseBtnClicked: function onCloseBtnClicked(ev) {
+        console.log("Close button clicked");
+        this.trigger('BUTTON_CLOSE_BTN_CLICKED', this.model.get('numId'));
+    },
+
+    getJSON: function getJSON() {
+        var btnJSON = {};
+        btnJSON["type"] = "web_url";
+        var title = this.$el.find('.btn-text').html();
+        console.log("The title is::" + title);
+        btnJSON["title"] = title;
+        btnJSON["url"] = "https://www.google.com"
+        return btnJSON;
+    }
+
+});
+
+
+var TextData = Backbone.Model.extend({
+    defaults: function() {
+        return {
+            "numId": 777
+        }
+    },
+
+    getNumId: function getNumId() {
+        return this.get('numId');
+    },
+
+    setNumId: function setNumId(numId) {
+        this.set({'numId': numId});
+    },
+
+    initialize: function () {
+    }
+});
+
+
+var TextView = Backbone.View.extend({
+    model: TextData,
+    
+
+    initialize: function () {
+        this.render();
+    },
+
+    events: {        
+        'click .text-close-button': 'onCloseBtnClicked',
+        
+    },
+
+    render: function () {
+        console.log('Inside render of TextView');        
+    },
+
+    onCloseBtnClicked: function onCloseBtnClicked(ev) {
+        console.log("Close button clicked");
+        this.trigger('TEXT_CLOSE_BTN_CLICKED', this.model.get('numId'));
+    },
+
+    getJSON: function getJSON() {
+        var textJSON = {};
+        
+        var title = this.$el.find('.text-text').html();
+        console.log("The title is::" + title);
+        textJSON["text"] = title;
+        return textJSON;
+    }
+
+});
+
 
 
 var model = new Data();
